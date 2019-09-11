0bdbcece29c514e45aa4c172eea957f1a5a2c081
admhemed <admhemed@gmail.com>
2019-09-11 12:11:31 +0300

    add doc

--- webpack/paths.js
+++ webpack/paths.js
@@ -6,6 +6,6 @@ const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 
 module.exports = {
   src: resolveApp('src'),
-  dist: resolveApp('dist'),
+  dist: resolveApp('docs'),
   static: resolveApp('static')
 }
